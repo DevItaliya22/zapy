4a9c562990b4e347793ff681f2a80456f1b80d11
Dev <devitaliya22@gmail.com>
2024-09-19 20:27:45 +0530

    change UI

--- src/app/(main)/(pages)/connections/page.tsx
+++ src/app/(main)/(pages)/connections/page.tsx
@@ -102,9 +102,6 @@ const Connections = async (props: Props) => {
 
   return (
     <div className="relative flex flex-col gap-4">
-      <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
-        Connections
-      </h1>
       <div className="relative flex flex-col gap-4">
         <section className="flex flex-col gap-4 p-6 text-muted-foreground">
           Connect all your apps directly from here. You may need to connect
